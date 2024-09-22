d05ab16bfa51618e08ec39654fc29d7c16b6801f
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-22 14:17:50 -0400

    drawgui

--- sketch.js
+++ sketch.js
@@ -2,14 +2,21 @@ let d = 60; //circle diameter
 let deg = 58;
 let outline = true;
 
+let gui;
+let diameterSlider, degSlider, outlineCheckbox;
+
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
   strokeWeight(10);
+
+  gui = createGui();
+  diameterSlider = createSlider("Diameter", 200, 10, 100, 20, 10, 100);
 }
 
 function draw() {
   background(125);
+  drawGui();
 
 if (outline) {
   stroke("black");
